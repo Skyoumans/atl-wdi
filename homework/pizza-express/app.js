7b7c5e1c1e399e24eb8ce8144df8ac0b0e695687
Skyoumans <skyyoumans@gmail.com>
2017-09-19 19:28:36 -0400

    completed GET routes

--- homework/pizza-express/app.js
+++ homework/pizza-express/app.js
@@ -5,3 +5,13 @@ const port = 3000;
 app.listen(port, function() {
     console.log("Listening on port " + port);
 });
+
+app.get('/', (req, res) => {
+    res.send("Welcome to Pizza Express!")
+});
+app.get('/topping/:type', (req, res, next) => {
+    res.send(req.params.type + " pizza! Great choice.");
+});
+app.get('/order/:amount/:size', (req, res, next) => {
+    res.send("Your order for " + req.params.amount + " " + req.params.size + " pizzas will be ready in 1 minute.")
+});
