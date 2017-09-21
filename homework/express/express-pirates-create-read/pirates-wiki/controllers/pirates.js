62d0032d62ebbc8b74f4f8f12e6214d873648df7
Skyoumans <skyyoumans@gmail.com>
2017-09-21 08:03:42 -0400

    finished homework

--- homework/express/express-pirates-create-read/pirates-wiki/controllers/pirates.js
+++ homework/express/express-pirates-create-read/pirates-wiki/controllers/pirates.js
@@ -1,1 +1,36 @@
-//your code below
+const express = require("express");
+const router = express.Router();
+const pirates = require('../models/pirates.js');
+
+
+/* Show Pirates Index */
+router.get('/', (req, res) => {
+    res.render('pirates/index', {
+        pirates: allThePirates
+    })
+})
+
+router.get('/new', (req, res) => {
+    res.render('pirates/new')
+})
+
+router.get('/:id', (req, res) => {
+    const id = parseInt(req.params.id);
+    const pirate = pirates.allThePirates[id];
+        res.render('pirates/show', {pirate})
+})
+
+router.post('/', (req, res) => {
+    console.log(req.body);
+    const newPirate = req.body;
+    pirates.allThePirates.push(newPirate);
+    res.redirect('/pirates');
+})
+
+
+// router.post('/', (req, res) => {
+//     const newPirate
+// })
+
+
+module.exports = router;
--- homework/express/express-pirates-create-read/pirates-wiki/server.js
+++ homework/express/express-pirates-create-read/pirates-wiki/server.js
@@ -1,1 +1,22 @@
-//your code here
+const express = require('express');
+const app = express();
+const hbs = require('hbs');
+const bodyParser = require('body-parser');
+const pirateController = require('./controllers/pirates.js');
+
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+app.set('view engine', 'hbs');
+
+app.use("/pirates", pirateController);
+
+
+
+
+
+const port = 3000
+app.listen(port, () => {
+    console.log("Express is listening on port", port);
+})
