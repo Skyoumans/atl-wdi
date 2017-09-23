99c519b78bc5216f37e00ef5f74266fb87536f63
Skyoumans <skyyoumans@gmail.com>
2017-09-23 14:50:38 -0400

    part 4: added new methods

--- homework/javaScript/js-constructor/tamagotchi.js
+++ homework/javaScript/js-constructor/tamagotchi.js
@@ -13,13 +13,25 @@ class Tamagotchi {
         this.foodInTummy--;
         console.log("WAHH!!!")
         console.log(this.name + ' has current food in tummy: ' + this.foodInTummy)
-    }
+    };
+    puke() {
+        this.foodInTummy--;
+        console.log(this.name + ' is feeling sick! Blehhhhh!')
+    };
+    yawn() {
+        this.restedness--;
+        console.log(this.name + ' has current restedness of ' + this.restedness)
+    };
 }
 
 const shiro = new Tamagotchi('Shiro', 'Alien')
 console.log(shiro)
 shiro.cry()
+shiro.puke()
+shiro.yawn()
 
 const timmy = new Tamagotchi('Timmy', 'Predator')
 console.log(timmy)
 timmy.cry()
+timmy.puke()
+timmy.yawn()
