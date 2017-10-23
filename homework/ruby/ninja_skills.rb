e2cdae863a1dab5a087a6454e0950e12124f3b1f
Skyoumans <skyyoumans@gmail.com>
2017-10-23 17:27:26 -0400

    2. Nil skills

--- homework/ruby/ninja_skills.rb
+++ homework/ruby/ninja_skills.rb
@@ -1,6 +1,16 @@
-# Challenge !: Booleans
+# Challenge 1: Booleans
 
-a = true
-b = false
-puts a == b
-puts a != b
+# a = true
+# b = false
+# puts a == b
+# puts a != b
+
+
+# Challenge 2: Nil
+
+# n = nil
+# p n
+# puts '#{n} was here'
+# print n
+# puts n.class
+
