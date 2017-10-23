7e6ca5cb5abe385d93376f49bc6219cfd40b1ad5
Skyoumans <skyyoumans@gmail.com>
2017-10-23 17:32:29 -0400

    4. Strings skills

--- homework/ruby/ninja_skills.rb
+++ homework/ruby/ninja_skills.rb
@@ -1,18 +1,17 @@
 # Challenge 1: Booleans
 
-# a = true
-# b = false
-# puts a == b
-# puts a != b
-
+a = true
+b = false
+puts a == b
+puts a != b
 
 # Challenge 2: Nil
 
-# n = nil
-# p n
-# puts '#{n} was here'
-# print n
-# puts n.class
+n = nil
+p n
+puts '#{n} was here'
+print n
+puts n.class
 
 # Challenge 3: Integers
 
@@ -21,3 +20,9 @@ puts d
 e = 3.1
 d = e
 puts d
+
+# Challenge 4. Strings
+
+christine = "Hi, I'm Christine"
+likes_to = "long walks on the beach, kinda"
+puts "#{christine} and I like #{likes_to}"
