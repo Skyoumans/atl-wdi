77771e6bde94fbd8602d3f67b5ceab68ffa2e1de
Skyoumans <skyyoumans@gmail.com>
2017-10-23 17:59:19 -0400

    6. Array skills

--- homework/ruby/ninja_skills.rb
+++ homework/ruby/ninja_skills.rb
@@ -1,40 +1,51 @@
-# Challenge 1: Booleans
+# # Challenge 1: Booleans
 
-a = true
-b = false
-puts a == b
-puts a != b
+# a = true
+# b = false
+# puts a == b
+# puts a != b
 
-# Challenge 2: Nil
+# # Challenge 2: Nil
 
-n = nil
-p n
-puts '#{n} was here'
-print n
-puts n.class
+# n = nil
+# p n
+# puts '#{n} was here'
+# print n
+# puts n.class
 
-# Challenge 3: Integers
+# # Challenge 3: Integers
 
-d = 10
-puts d
-e = 3.1
-d = e
-puts d
+# d = 10
+# puts d
+# e = 3.1
+# d = e
+# puts d
 
-# Challenge 4. Strings
+# # Challenge 4. Strings
 
-christine = "Hi, I'm Christine"
-likes_to = "long walks on the beach, kinda"
-puts "#{christine} and I like #{likes_to}"
+# christine = "Hi, I'm Christine"
+# likes_to = "long walks on the beach, kinda"
+# puts "#{christine} and I like #{likes_to}"
 
-# Challenge 5. Input and Output
+# # Challenge 5. Input and Output
 
-puts "Input a Number"
-number = gets.chomp
-puts number.to_i * 2
+# puts "Input a Number"
+# number = gets.chomp
+# puts number.to_i * 2
 
-puts "What is your name?"
-name = gets.chomp
-puts "What do you like to do?"
-likes = gets.chomp
-puts "#{name} likes to #{likes}"
+# puts "What is your name?"
+# name = gets.chomp
+# puts "What do you like to do?"
+# likes = gets.chomp
+# puts "#{name} likes to #{likes}"
+
+# Challenge 6: Arrays
+
+my_first_array = []
+puts my_first_array = ["What", "are", "we", "doing", "here", 1,2,3,4,5]
+puts my_first_array.last
+puts my_first_array.first
+puts my_first_array.insert(1, "Joe")
+puts my_first_array.insert(2, 3)
+puts my_first_array.push("true")
+puts my_first_array.push[3]
